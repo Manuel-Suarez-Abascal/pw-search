5bf8c7a1a0c7e0ab469352dbbf1ba97b2e6b8471
Manuel Abascal <manuelsuarezabascal@gmail.com>
2023-06-12 08:01:02 -0400

    added some UX improvements

--- bin/index.js
+++ bin/index.js
@@ -1,10 +1,39 @@
 import chalk from 'chalk';
 import fetch from 'node-fetch';
-import readline from 'readline';
+import keypress from 'keypress';
+import { stdout } from 'process';
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+// make `process.stdin` begin emitting "keypress" events
+keypress(process.stdin);
+
+const searchPrompt = '🔍 Please enter a search query: ';
+const exitPrompt = 'Press CTRL + C to exit search.';
+let searchString = '';
+
+console.log(chalk.green(searchPrompt));
+
+// Hide the cursor
+process.stdout.write("\x1B[?25l");
+
+// listen for the "keypress" event
+process.stdin.on('keypress', function (ch, key) {
+  if (key && key.ctrl && key.name == 'c') {
+    process.exit();
+  } else if (key && key.name == 'backspace') {
+    searchString = searchString.slice(0, searchString.length - 1);
+  } else if (ch) {
+    searchString += ch;
+  }
+
+  stdout.write('\x1Bc'); // Clear console
+  console.log(chalk.green(searchPrompt), searchString);
+
+  // Hide the cursor
+  process.stdout.write("\x1B[?25l");
+
+  if (searchString.length > 0) {
+    performSearch(searchString);
+  }
 });
 
 // function to perform the search in the CLI and print the results
@@ -14,7 +43,7 @@ const performSearch = async (queryTerm) => {
     "requests": [
       {
         "indexName": "playwright-nodejs",
-        "params": `query=${queryTerm}`
+        "params": `query=${queryTerm}&hitsPerPage=5`
       }
     ]
   };
@@ -23,9 +52,6 @@ const performSearch = async (queryTerm) => {
   let options = {
     method: 'POST',
     headers: {
-      /* these headers are required for the request to work in Algolia API 
-      // it's not sensitive data since it's available in the browser network tab for Playwright official docs
-      */
       'Content-Type': 'application/json',
       'X-Algolia-API-Key': 'a5b64422711c37ab6a0ce4d86d16cdd9',
       'X-Algolia-Application-Id': 'K09ICMCV6X'
@@ -37,20 +63,41 @@ const performSearch = async (queryTerm) => {
   fetch('https://k09icmcv6x-dsn.algolia.net/1/indexes/*/queries', options)
     .then(response => response.json())
     .then(data => {
-
       // If no results are found from the search, print a message and stop execution
       if (data.results[0].nbHits === 0) {
+
         console.log(chalk.red('No results found!'));
+
+        // Hide the cursor
+        process.stdout.write("\x1B[?25l");
         return;
       }
 
       // Print the number of results found & their urls to the playwright docs
-      data.results[0].hits.forEach(hit => console.log(chalk.yellow(hit.url_without_anchor)) );
+      data.results[0].hits.forEach(hit => {
+        const baseURL = hit.url_without_anchor;
+        console.log(chalk.yellow(baseURL));
+  
+        Object.values(hit.hierarchy).forEach(section => {
+          if (section) {
+            let currentURL = `${baseURL}#${section.toLowerCase().split(' ').join('-')}`;
+            console.log(chalk.yellow(currentURL));
+          }
+        });
+      });
+      
+      console.log(chalk.red(exitPrompt));
+
+      // Hide the cursor
+      process.stdout.write("\x1B[?25l");
     })
     .catch(error => console.error('Error:', error));
 }
 
-rl.question(chalk.green('🔍 Please enter a search query: '), (answer) => {
-  performSearch(answer);
-  rl.close();
+process.stdin.setRawMode(true);
+process.stdin.resume();
+
+// Make sure to show the cursor before your application exits
+process.on('exit', () => {
+  process.stdout.write("\x1B[?25h");
 });
